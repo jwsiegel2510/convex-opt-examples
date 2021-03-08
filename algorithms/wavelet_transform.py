a690f9a0e795cb9880355da6e426aa468b3a3c48
Jonathan Siegel <jus1949.psu.edu>
2021-03-07 21:38:14 -0500

    Added higher order daubechies wavelets and an implementation of douglas rachford for minimizing wavelet l1 norm with gives pixel constraints

--- algorithms/wavelet_transform.py
+++ algorithms/wavelet_transform.py
@@ -5,75 +5,154 @@
 
 import numpy as np
 from numpy.random import rand
+from numpy.polynomial import Polynomial as P
 import math
 
-principal_filters = {'haar': np.array([1.0 / math.sqrt(2), 1.0 / math.sqrt(2)])}
-second_filters = {'haar': np.array([1.0 / math.sqrt(2), -1.0 / math.sqrt(2)])}
+# Calculate and cache wavelet filters. Uses polynomial roots, reasonably stable up to order 10.
+principal_filters = {}
+wavelet_filters = {}
+def get_filters(order):
+    if order not in principal_filters or order not in wavelet_filters:
+        # Calculate principal filter.
+        principal_filter = None
+        if order == 0:
+            principal_filter = np.array([1.0, 1.0])
+            principal_filter /= np.linalg.norm(principal_filter)
+        else:
+            # Calculate binomial coefficients.
+            bin_coeff = np.zeros([order + 2])
+            bin_coeff[0] = 1.0
+            for i in range(order + 1):
+                for j in range(order, -1, -1):
+                    bin_coeff[j+1] += bin_coeff[j]
+            # Set up coefficient matrix.
+            matrix = np.matrix(np.zeros([order + 1, order + 1]))
+            for i in range(order + 1):
+                for j in range(1 + 2 * i):
+                    if j < order + 2 and 2*i - j < order + 1:
+                        matrix[i, 2*i - j] += bin_coeff[j]
+            # Solve for the coefficients of q(z)*q(z^-1)
+            target = np.zeros([order + 1])
+            target[0] = 1.0
+            coeffs = np.linalg.solve(matrix, target)
+            # Construct polynomial and find roots.
+            poly = P(coeffs)
+            roots = poly.roots()
+            # Determine filter based on roots.
+            principal_filter = np.zeros([2 * order + 2], dtype=complex)
+            principal_filter[0] = 1
+            for root in roots:
+                # Only consider real roots and imaginary roots with positive real part.
+                if root.imag > -1e-10:
+                    z = root + np.sqrt(root ** 2 - 1)
+                    for j in range(2 * order + 1, 0, -1):
+                        principal_filter[j] = principal_filter[j-1] - z * principal_filter[j]
+                    principal_filter[0] *= (-z)
+                # If the root is imaginary, take the conjugate of z as well.
+                if root.imag > 1e-10:
+                    z = np.conjugate(z)
+                    for j in range(2 * order + 1, 0, -1):
+                        principal_filter[j] = principal_filter[j-1] - z * principal_filter[j]
+                    principal_filter[0] *= (-z)
+            principal_filter = principal_filter.real
+            for i in range(order + 1):
+                for j in range(2 * order + 1, 0, -1):
+                    principal_filter[j] = principal_filter[j-1] + principal_filter[j]
+            principal_filter /= np.linalg.norm(principal_filter)
+        # Obtain the wavelet filter by reversing and alternating signs.
+        wavelet_filter = np.zeros_like(principal_filter)
+        for j in range(principal_filter.size):
+            wavelet_filter[j] = principal_filter[principal_filter.size - j - 1]
+            if j%2 == 1:
+                wavelet_filter[j] *= -1
+        principal_filters[order] = principal_filter
+        wavelet_filters[order] = wavelet_filter
+    return {'principal': principal_filters[order], 'wavelet': wavelet_filters[order]}
+
+#s = math.sqrt(3)
+#r = (1 + s) / (1 - s)
+#d2_unorm = np.array([r, r*s, -1.0*s, 1.0])
+
+#principal_filters = {'haar': (1.0 / math.sqrt(2)) * np.array([1.0, 1.0]),
+#                     'daubechies2': (1.0 / np.linalg.norm(d2_unorm)) * d2_unorm}
+#second_filters = {'haar': (1.0 / math.sqrt(2)) * np.array([1.0, -1.0]),
+#                  'daubechies2': (1.0 / np.linalg.norm(d2_unorm)) * np.array([d2_unorm[3], -1.0*d2_unorm[2], d2_unorm[1], -1.0*d2_unorm[0]])}
 
 # Wavelet transform. Assumes the signal length is a power of 2. Performs the transform for each row.
-def wavelet_transform_1d(signal, wavelet_type='haar'):
+def wavelet_transform_1d(signal, wavelet_order=0):
     if signal.shape[1] <= 1:
         return signal
     if signal.shape[1] % 2 != 0:
         print('An error occurred in wavelet transform. Size not a power of 2.')
     transform = np.zeros_like(signal)
-    principal_filter = principal_filters[wavelet_type]
-    second_filter = second_filters[wavelet_type]
+    filters = get_filters(wavelet_order)
+    principal_filter = filters['principal']
+    wavelet_filter = filters['wavelet']
     for i in range(signal.shape[1] // 2):
         for j in range(principal_filter.size):
             transform[:, i] += principal_filter[j] * signal[:, (2*i + j) % signal.shape[1]]
-            transform[:, signal.shape[1] // 2 + i] += second_filter[j] * signal[:, (2*i + j) % signal.shape[1]]
-    transform[:, 0:signal.shape[1] // 2] = wavelet_transform_1d(transform[:, 0:signal.shape[1] // 2], wavelet_type)
+            transform[:, signal.shape[1] // 2 + i] += wavelet_filter[j] * signal[:, (2*i + j) % signal.shape[1]]
+    transform[:, 0:signal.shape[1] // 2] = wavelet_transform_1d(transform[:, 0:signal.shape[1] // 2], wavelet_order)
     return transform
 
 # Inverse wavelet transform. Assumes the signal length is a power of 2. Performs the inverse transform for each row.
-def inv_wavelet_transform_1d(transform, wavelet_type='haar'):
+def inv_wavelet_transform_1d(transform, wavelet_order=0):
     if transform.shape[1] <= 1:
         return transform
     if transform.shape[1] % 2 != 0:
         print('An error occurred in wavelet transform. Size not a power of 2.')
-    transform[:, 0:(transform.shape[1] // 2)] = inv_wavelet_transform_1d(transform[:, 0:(transform.shape[1] // 2)], wavelet_type)
+    transform[:, 0:(transform.shape[1] // 2)] = inv_wavelet_transform_1d(transform[:, 0:(transform.shape[1] // 2)], wavelet_order)
     signal = np.zeros_like(transform)
-    principal_filter = principal_filters[wavelet_type]
-    second_filter = second_filters[wavelet_type]
+    filters = get_filters(wavelet_order)
+    principal_filter = filters['principal']
+    wavelet_filter = filters['wavelet']
     for i in range(signal.shape[1]):
         for j in range(principal_filter.size // 2):
             index = (((i // 2) - j)%(transform.shape[1] // 2) + (transform.shape[1] // 2)) % (transform.shape[1] // 2)
             signal[:, i] += principal_filter[(i%2) + 2*j] * transform[:, index]
-            signal[:, i] += second_filter[(i%2) + 2*j] * transform[:, index + (transform.shape[1] // 2)]
+            signal[:, i] += wavelet_filter[(i%2) + 2*j] * transform[:, index + (transform.shape[1] // 2)]
     return signal
 
 # Wavelet transform in 2D. Assumes both image dimensions are a power of 2.
-def wavelet_transform_2d(image, wavelet_type='haar'):
-    image = wavelet_transform_1d(image, wavelet_type)
-    image = np.transpose(wavelet_transform_1d(np.transpose(image), wavelet_type))
+def wavelet_transform_2d(image, wavelet_order=0):
+    image = wavelet_transform_1d(image, wavelet_order)
+    image = np.transpose(wavelet_transform_1d(np.transpose(image), wavelet_order))
     return image
 
 # Inverse wavelet transform in 2D. Assumes both image dimension are a power of 2.
-def inv_wavelet_transform_2d(transform, wavelet_type='haar'):
-    transform = np.transpose(inv_wavelet_transform_1d(np.transpose(transform), wavelet_type))
-    transform = inv_wavelet_transform_1d(transform, wavelet_type)
+def inv_wavelet_transform_2d(transform, wavelet_order=0):
+    transform = np.transpose(inv_wavelet_transform_1d(np.transpose(transform), wavelet_order))
+    transform = inv_wavelet_transform_1d(transform, wavelet_order)
     return transform
 
 # Calculate the l1 norm of the wavelet coefficients.
-def wavelet_l1(image, wavelet_type='haar'):
-    transform = wavelet_transform_2d(image, wavelet_type)
+def wavelet_l1(image, wavelet_order=0):
+    transform = wavelet_transform_2d(image, wavelet_order)
     return np.sum(np.abs(transform))
 
 # Calculate the wavelet shrink.
-def wavelet_l1_prox(image, wavelet_type='haar', lam=1.0):
-    transform = wavelet_transform_2d(image, wavelet_type)
+def wavelet_l1_prox(image, wavelet_order=0, lam=1.0):
+    transform = wavelet_transform_2d(image, wavelet_order)
     transform = np.multiply(np.sign(transform), np.multiply((np.absolute(transform) - lam > 0), np.absolute(transform) - lam))
-    return inv_wavelet_transform_2d(transform, wavelet_type)
+    return inv_wavelet_transform_2d(transform, wavelet_order)
 
 # Tests the wavelet transforms.
 def main():
+    # k is the order to test.
+    k = 10
+    filters = get_filters(k)
+    print(filters)
+    wavelet_filter = filters['wavelet']
+    vect = np.ones_like(wavelet_filter)
+    for i in range(k+1):
+        print(np.inner(vect, wavelet_filter))
+        for j in range(2*k+2):
+            vect[j] *= (j-i)
     array = rand(512, 512)
-    transform = wavelet_transform_2d(array)
+    transform = wavelet_transform_2d(array, wavelet_order=k)
     print(np.linalg.norm(array))
     print(np.linalg.norm(transform))
-    print(np.linalg.norm(inv_wavelet_transform_2d(transform) - array))
-    print(wavelet_l1_prox(array, lam=0.1))
+    print(np.linalg.norm(inv_wavelet_transform_2d(transform, wavelet_order=k) - array))
 
 if __name__ == '__main__':
     main()
--- reconstruction_example.py
+++ reconstruction_example.py
@@ -1,13 +1,11 @@
 # Author: Jonathan Siegel
 #
-# Contains a rudimentary inpainting and denoising example to play around with.
+# Contains a rudimentary inpainting example to play around with.
 
 from utils import load_image, plot_image
 import numpy as np
 from numpy.random import random_sample as rand
 from algorithms import reconstruct, trivial_reconstruct
-
-loss_frac = 0.9
 
 def main():
     # Load and plot original image.
@@ -17,13 +15,17 @@ def main():
 
     # Randomly delete pixels from the original image.
     print('\nMany pixel values have been lost.\nHere is the corrupted image.')
-    mask = np.double(rand(np.shape(image)) > loss_frac)
+    mask = np.ones_like(image)
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            if i%40 < 30 and j % 40 < 30:
+                mask[i][j] = 0
     corr_image = np.multiply(image, mask)
     plot_image(corr_image)
 
     # Reconstruct the image
     print('\nPerforming image reconstruction.')
-    rec_image = trivial_reconstruct(corr_image, mask) #, lam = 0.03, max_iter=100)
+    rec_image = reconstruct(corr_image, mask, lam = 0.3, max_iter=50, alg='dr', wavelet_order=4)
     print('Reconstruction complete!')
     print('Displaying Reconstructed Image.')
     plot_image(rec_image)
