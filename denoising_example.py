0b245c7be7d12598aa81e462d15105bbff3b914c
Jonathan Siegel <jus1949.psu.edu>
2021-03-08 16:46:36 -0500

    Changed denoising to Mickey image

--- denoising_example.py
+++ denoising_example.py
@@ -7,12 +7,12 @@ import numpy as np
 from numpy.random import random_sample as rand
 from algorithms import wavelet_l1_prox
 
-noise_level = 0.5
+noise_level = 0.8
 
 def main():
     # Load and plot original image.
     print('Load and displaying original image.')
-    image = load_image(filename='lenna-image.jpeg')
+    image = load_image()
     plot_image(image)
 
     # Randomly sample entries of the Fourier transform of the signal.
@@ -22,7 +22,7 @@ def main():
 
     # Reconstruct the image
     print('\nPerforming image denoising using wavelets.')
-    rec_image = wavelet_l1_prox(image, lam=0.65, wavelet_order=3)
+    rec_image = wavelet_l1_prox(image, lam=1.0, wavelet_order=3)
     print('Denoising complete!')
     print('Displaying Denoised Image.')
     plot_image(np.clip(rec_image, 0, 1))
