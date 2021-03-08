96f7e5908935fdeefc854e69212d4f1c916cd840
Jonathan Siegel <jus1949.psu.edu>
2021-03-08 16:28:27 -0500

    Added a denosing example

---
+++ denoising_example.py
@@ -0,0 +1,31 @@
+# Author: Jonathan Siegel
+#
+# Contains an example of image denoising using wavelets.
+
+from utils import load_image, plot_image
+import numpy as np
+from numpy.random import random_sample as rand
+from algorithms import wavelet_l1_prox
+
+noise_level = 0.5
+
+def main():
+    # Load and plot original image.
+    print('Load and displaying original image.')
+    image = load_image(filename='lenna-image.jpeg')
+    plot_image(image)
+
+    # Randomly sample entries of the Fourier transform of the signal.
+    print('\nImage has been corrupted and made noisy.')
+    image = image + np.random.normal(scale=noise_level, size=image.shape)
+    plot_image(np.clip(image, 0, 1))
+
+    # Reconstruct the image
+    print('\nPerforming image denoising using wavelets.')
+    rec_image = wavelet_l1_prox(image, lam=0.65, wavelet_order=3)
+    print('Denoising complete!')
+    print('Displaying Denoised Image.')
+    plot_image(np.clip(rec_image, 0, 1))
+
+if __name__ == '__main__':
+    main()
