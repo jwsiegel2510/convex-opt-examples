133af67b34ee161c759e0cb8423d4e9a70ce8a93
Jonathan Siegel <jus1949.psu.edu>
2021-03-09 11:17:45 -0500

    Added back a wavelet inpaining example and removed unnecessary files

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,3 +1,4 @@
 from .wavelet_reconstruct import reconstruct_danzig, reconstruct_lasso
+from .wavelet_inpaint import inpaint_danzig, inpaint_lasso
 from .wavelet_transform import wavelet_l1, wavelet_l1_prox
 from .trivial_inpaint import trivial_inpaint
--- inpainting_example.py
+++ inpainting_example.py
@@ -8,31 +8,28 @@
 from utils import load_image, plot_image
 import numpy as np
 from numpy.random import random_sample as rand
-from algorithms import reconstruct_danzig, reconstruct_lasso
+from algorithms import inpaint_danzig, inpaint_lasso, trivial_inpaint
 
-sample_frac = 0.70
+sample_frac = 0.3
 
 def main():
     # Load and plot original image.
     print('Load and displaying original image.')
-    image = load_image(filename='lenna-image.jpeg')
+    image = load_image()
     plot_image(image)
 
     # Randomly sample entries of the Fourier transform of the signal.
-    print('\n%2.0f%% of the Fourier transform of the image has been sampled.' % (sample_frac*100))
-    transform = np.fft.rfftn(image, norm='ortho')
-    mask = np.double(rand(transform.shape) < sample_frac)
-    sampled_transform = np.multiply(transform, mask)
-    print('Displaying image obtained by setting remaining Fourier modes to 0.')
-    print('Note that this image fits the samples while minimizing the wavelet l2 norm.')
-    plot_image(np.fft.irfftn(sampled_transform, norm='ortho'))
+    print('\n%2.0f%% of the image has been sampled.' % (sample_frac*100))
+    mask = np.double(rand(image.shape) < sample_frac)
+    sampled_image = np.multiply(mask, image)
+    plot_image(sampled_image)
 
     # Reconstruct the image
     print('\nPerforming image reconstruction.')
-    rec_image = reconstruct_lasso(sampled_transform, mask, lam=0.05, max_iter=50, wavelet_order=2)
+    rec_image = inpaint_danzig(sampled_image, mask, step=0.3, max_iter=50, wavelet_order=2)
     print('Reconstruction complete!')
     print('Displaying Reconstructed Image.')
-    plot_image(rec_image)
+    plot_image(np.clip(rec_image, 0, 1))
 
 if __name__ == '__main__':
     main()
