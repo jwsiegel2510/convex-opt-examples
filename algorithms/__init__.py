9bb57d549d7cc89fafda2d787af2fd874abc1cf6
Jonathan Siegel <jus1949.psu.edu>
2021-05-04 14:07:28 -0400

    Added solution to the total variation project

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,2 +1,3 @@
 from .wavelet_transform import wavelet_l1, wavelet_l1_prox, wavelet_transform_2d, inv_wavelet_transform_2d
 from .compressed_sensing import sparse_recover
+from .total_variation import total_variation_denoising
